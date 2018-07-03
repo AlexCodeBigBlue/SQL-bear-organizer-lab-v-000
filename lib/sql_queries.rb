addb2d1553ab46ab14c08d25acf67d487d05be56
Alex Schwerin <acschwerin@gmail.com>
2018-07-03 21:17:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender = 'F';"
 end
@@ -19,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "Select color, COUNT(color) FROM bears GROUP BY color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
